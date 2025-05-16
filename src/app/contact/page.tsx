3b0e1e106c3ab5e2d40c1afc0d4706f52d1cb8c2
higashida <higashida@insel-art.jp>
2025-05-16 13:05:54 +0900

    next link設定

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,8 +1,8 @@
+import Link from "next/link";
 export default function ContactPage() {
   return (
     <>
-
-      <a href="/" data-transition>top</a>
+      <Link href="/">トップに戻る</Link>
       <h1>お問い合わせ</h1>
     </>
   );
